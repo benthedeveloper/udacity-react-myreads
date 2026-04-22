5a795cd6325802ffd24dab3ef2440510b5cbd854
Ben Harrison <benthedeveloper@users.noreply.github.com>
2026-04-22 13:10:52 -0700

    fix: Set basename on BrowserRouter so app should run on production and locally correctly.

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL || ''}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
